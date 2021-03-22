935545faf99606a5231e1ffa0f5e5458b80d6657
garmanovvladimir <vova.garmanoff@yandex.ru>
2021-03-22 18:22:14 +0500

    - small fixes

--- app/src/main/java/com/example/movieinfotest/old/MovieFrom.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movieinfotest.old
-
-
-data class MovieFrom<T>(val movie: T, var isFromFavorite: Boolean) {
-    companion object {
-        const val FAVORITE = true
-        const val REGULAR = false
-    }
-}
