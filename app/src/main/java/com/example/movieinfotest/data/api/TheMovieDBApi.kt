4351cb012aa91fc05369d648acb848207430645b
garmanovvladimir <vova.garmanoff@yandex.ru>
2021-05-21 18:17:11 +0500

    [add] added logging interceptor

--- app/src/main/java/com/example/movieinfotest/data/api/TheMovieDBApi.kt
+++ app/src/main/java/com/example/movieinfotest/data/api/TheMovieDBApi.kt
@@ -1,15 +1,19 @@
 package com.example.movieinfotest.data.api
 
+import android.util.Log
 import com.example.movieinfotest.data.entities.actors.FilmActors
 import com.example.movieinfotest.data.entities.details.MovieDetails
 import com.example.movieinfotest.data.entities.genre.GenreCollection
 import com.example.movieinfotest.data.entities.popular.PopularFilms
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface TheMovieDBApi {
 
@@ -51,10 +55,24 @@ interface TheMovieDBApi {
         private const val BASE_URL = "https://api.themoviedb.org/3/"
 
         fun create(): TheMovieDBApi {
+            val loger = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+                override fun log(message: String) {
+                    Log.d("OkHttp", message)
+                }
+            })
+            loger.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .connectTimeout(2000, TimeUnit.SECONDS)
+                .writeTimeout(2000, TimeUnit.SECONDS)
+                .readTimeout(2000, TimeUnit.SECONDS)
+                .addInterceptor(loger)
+                .build()
 
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
                 .create(TheMovieDBApi::class.java)
         }
